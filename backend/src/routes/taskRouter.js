16e1da91769d7dce6a06937202ad594dfa2d8cc6
igor <infingardidev@gmail.com>
2022-07-03 20:14:36 -0300

    feat: adicionando rotas para o crud de tarefas

--- backend/src/routes/taskRouter.js
+++ backend/src/routes/taskRouter.js
@@ -4,5 +4,9 @@ const controller = require('../controllers/taskController');
 const taskRouter = Router();
 
 taskRouter.get('/tasks', controller.getAllTasks);
+taskRouter.get('/tasks/:id', controller.getTaskById);
+taskRouter.post('/tasks', controller.createTask);
+taskRouter.put('/tasks/:id', controller.updateTask);
+taskRouter.delete('/tasks/:id', controller.deleteTask);
 
 module.exports = taskRouter;
