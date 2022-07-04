07f349ec2f8fa88fef3c102d6bfbe57b06f8b001
igor <infingardidev@gmail.com>
2022-07-03 22:16:55 -0300

    feat: adicionando autenticação com token jwt

---
+++ backend/src/middlewares/authToken.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+
+const authToken = (req, res, next) => {
+  try {
+    const token = req.headers.authorization;
+
+    if (!token) return res.status(401).json({ message: 'Token not found' });
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.user = decoded;
+
+    return next();
+  } catch (error) {
+    return res.status(401).json({ message: 'Expired or invalid token' });
+  }
+};
+
+module.exports = authToken;
